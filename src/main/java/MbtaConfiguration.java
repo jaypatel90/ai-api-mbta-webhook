4798efb54c8bce813c985e53da14613bf58e1dc3
jaypatel90 <pjay5344@gmail.com>
2016-12-08 21:38:23 -0500

    init commit

---
+++ src/main/java/MbtaConfiguration.java
@@ -0,0 +1,12 @@
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbtaConfiguration {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MbtaController.class, args);
+    }
+
+}
